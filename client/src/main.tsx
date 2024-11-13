46995f718ed79300916ad40de108f64ce433f5c7
Ruben Dominguez <71775213+RubenDguez@users.noreply.github.com>
2024-11-13 12:28:18 -0500

    Feature/color palette (#24)
    * remove: default font from app css
    
    * add: theme provider to the whole app
    
    This is where material ui is expecting theme configurations
    
    * fix: login when wrong credentials
    
    * add: optimization for mui emotion

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,5 +6,12 @@ import '@fontsource/roboto/700.css';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import theme from './theme';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <RouterProvider router={router} />
+  </ThemeProvider>,
+);
